69584ec00c04ab89e708e915efb4f2d567da85cb
Kevin Hu <hukxchen+git@gmail.com>
2020-10-09 16:03:48 -0500

    dep: add lib-jitsi-meet via script tag
    because doing it as an ES6 module is just too dang hard

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,33 @@ import { hot } from 'react-hot-loader';
 import React from 'react';
 import './App.css';
 
+const loadAndConnect = ({ domain, room }) => new Promise((resolve) => {
+  const script = document.createElement('script');
+  script.src = `https://${domain}/libs/lib-jitsi-meet.min.js`;
+  document.querySelector('head').appendChild(script);
+
+  script.onload = () => {
+    JitsiMeetJS.init();
+
+    const configScript = document.createElement('script');
+    configScript.src = `https://${domain}/config.js`;
+    document.querySelector('head').appendChild(configScript);
+    // configScript.onload = () => {
+    //   connectandJoin({ domain, room, config }).then(resolve);
+    // };
+  };
+
+});
+
 const message = 'Welcome to vinto';
-const App = () => (
-  <div className="App">
-    <h1>{message}</h1>
-  </div>
-);
+const App = () => {
+  loadAndConnect({domain: 'meet.jit.si', room: 'some-default-room'});
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+};
 
 export default hot(module)(App);
