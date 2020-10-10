baaf3b38d05f0e573086b897acf5f405a54469be
Chikara <chtakahashi10@gmail.com>
2020-10-10 10:00:01 -0400

    feat: adds a video component and an index.js within the components folder for concise import/exports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { hot } from 'react-hot-loader';
 import React, { useState, useCallback } from 'react';
+import { Video } from './components'
 import $ from 'jquery';
 
 window.$ = $;
@@ -53,7 +54,6 @@ const App = () => {
   const [currentConf, setConference] = useState(null);
   // for our purposes, we just want to connect immediately upon load for now.
   const { conference, localVideoTrack } = loadAndConnect({ domain: 'meet.jit.si', room: 'some-default-room' });
-  // !! NEED TO WRAP setConference in a useEffect hook - otherwise infinite re-renderings happen each time we attempt to load and Connect. Commented out everything below for now, time to sleep!
   // setConference(conference);
   // const [videoTracks, addVideoTrack, removeVideoTrack] = useTracks();
   // const [audioTracks, addAudioTrack, removeAudioTrack] = useTracks();
@@ -71,7 +71,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>{message}</h1>
-      {currentConf ? <h2>There is an active conference</h2> : <h2>There is no active conference</h2>}
+      <Video />
     </div>
   );
 };
---
+++ src/components/video.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Video = (props) => (
+    <div>
+      This would be an individual video window!
+    </div>
+);
+
+export default Video
