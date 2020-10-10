5e1538331813297e44eb51301d605ac249561891
Chikara <chtakahashi10@gmail.com>
2020-10-09 23:07:25 -0400

    fix: conference and localVideoTrack errors were fixed by passing localVideoTrack into the connectionSuccessful callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { hot } from 'react-hot-loader';
-import React from 'react';
-import $ from 'jquery'
 import './App.css';
-window.$ = $
+import { hot } from 'react-hot-loader';
+import React, { useState, useCallback } from 'react';
+import $ from 'jquery';
 
-function connectionSuccessful(room, conference) {
-  console.log('=============> CONNECTION SUCCESSFUL <============');
+window.$ = $;
+
+async function connectionSuccessful(room, conference, localVideoTrack) {
+  console.log('=============> CONNECTION SUCCESSFUL <=============');
   conference = this.initJitsiConference(room, {});
-  // conference.on();
-};
+  conference.on(JitsiMeetJS.events.conference.CONFERENCE_JOINED, () => console.log('!!!!!!!CONFERENCE JOINED'));
+  conference.join();
+  const localTracks = await JitsiMeetJS.createLocalTracks({ devices: ['video', 'audio'], facingMode: 'user' }, true);
+  localVideoTrack = localTracks.find((track) => track.getType() === 'video');
+  const localAudioTrack = localTracks.find((track) => track.getType() === 'audio');
+  conference.addTrack(localVideoTrack);
+  conference.addTrack(localAudioTrack);
+  console.log('=============> Video & Audio connected <=============');
+}
 
 const connectionFailed = () => {
-  console.log('=============> CONNECTION FAILED <============');
+  console.log('=============> CONNECTION FAILED <=============');
 };
 
 const loadAndConnect = async ({ domain, room }) => {
@@ -20,31 +28,50 @@ const loadAndConnect = async ({ domain, room }) => {
   document.querySelector('head').appendChild(script);
 
   let conference;
-  script.onload = () => {
+  let localVideoTrack;
+  script.onload = async () => {
     JitsiMeetJS.init();
-
     const configScript = document.createElement('script');
     configScript.src = `https://${domain}/config.js`;
     document.querySelector('head').appendChild(configScript);
     configScript.onload = async () => {
-      config.serviceUrl = config.websocket || config.bosh
-      config.serviceUrl += `?room=${room}`
-      const connection = new JitsiMeetJS.JitsiConnection(null, undefined, config)
-      connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED, connectionSuccessful.bind(connection, room, conference))
-      connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_FAILED, connectionFailed)
+      config.serviceUrl = config.websocket || config.bosh;
+      config.serviceUrl += `?room=${room}`;
+      const connection = new JitsiMeetJS.JitsiConnection(null, undefined, config);
+      connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED,
+        connectionSuccessful.bind(connection, room, conference, localVideoTrack));
+      connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_FAILED,
+        connectionFailed);
       await connection.connect();
-    }
+    };
   };
-  return conference
+  return { conference, localVideoTrack };
 };
 
 const message = 'Welcome to vinto';
 const App = () => {
-  loadAndConnect({ domain: 'meet.jit.si', room: 'some-default-room' });
+  const [currentConf, setConference] = useState(null);
+  // for our purposes, we just want to connect immediately upon load for now.
+  const { conference, localVideoTrack } = loadAndConnect({ domain: 'meet.jit.si', room: 'some-default-room' });
+  // !! NEED TO WRAP setConference in a useEffect hook - otherwise infinite re-renderings happen each time we attempt to load and Connect. Commented out everything below for now, time to sleep!
+  // setConference(conference);
+  // const [videoTracks, addVideoTrack, removeVideoTrack] = useTracks();
+  // const [audioTracks, addAudioTrack, removeAudioTrack] = useTracks();
+
+  // const addTrack = useCallback((track) => {
+  //   if (track.getType() === 'video') addVideoTrack(track);
+  //   if (track.getType() === 'audio') addAudioTrack(track);
+  // }, [addVideoTrack, addAudioTrack]);
+
+  // const removeTrack = useCallback((track) => {
+  //   if (track.getType() === 'video') removeVideoTrack(track);
+  //   if (track.getType() === 'audio') removeAudioTrack(track);
+  // }, [removeAudioTrack, removeVideoTrack]);
 
   return (
     <div className="App">
       <h1>{message}</h1>
+      {currentConf ? <h2>There is an active conference</h2> : <h2>There is no active conference</h2>}
     </div>
   );
 };
