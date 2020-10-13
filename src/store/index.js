5fb23acf2eeb1fd737f1580b76aad18f1b9976b4
Kevin Hu <hukxchen+git@gmail.com>
2020-10-12 20:20:30 -0500

    feat: add participants slice of state

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import engagementScore from './engagementScore'
+import participants from './participants'
 
 const reducer = combineReducers({engagementScore, participants})
 const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
