c0fe044d9d5dc991328fe39c643d038e478cac12
Kevin Hu <hukxchen+git@gmail.com>
2020-10-13 11:06:08 -0500

    feat: make participants reducer available

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,10 @@ import thunkMiddleware from 'redux-thunk'
 import engagementScore from './engagementScore'
 import participants from './participants'
 
-const reducer = combineReducers({engagementScore})
+const reducer = combineReducers({engagementScore, participants})
 const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 const store = createStore(reducer, middleware)
 
 export default store
 export * from './engagementScore'
+export * from './participants'
