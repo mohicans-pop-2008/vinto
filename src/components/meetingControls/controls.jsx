a77728c1f0641c608abce34b45dd178f3e8463e3
taveershah <tanvirhussein@gmail.com>
2020-10-15 16:07:45 -0400

    feat: emoji component added

--- src/components/meetingControls/controls.jsx
+++ src/components/meetingControls/controls.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ScreenShare from './screenShare';
 import Mute from './mute';
 import RaiseHand from './raiseHand';
+import Emoji from './emoji';
 
 const Controls = (props) => {
   const { name, toggleMute, uniqueID } = props;
   return (
     <div>
       <ScreenShare />
-      <Mute toggleMute={toggleMute} trackType='video' />
-      <Mute toggleMute={toggleMute} trackType='audio' />
+      <Mute toggleMute={toggleMute} trackType="video" />
+      <Mute toggleMute={toggleMute} trackType="audio" />
       <RaiseHand name={name} uniqueID={uniqueID} />
+
+      <Emoji />
     </div>
   );
 };
