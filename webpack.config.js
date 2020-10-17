0a9c21fce63f277b28e5c2d3333e13fc89045256
Kevin Hu <hukxchen+git@gmail.com>
2020-10-17 11:37:23 -0500

    retool: implements custom webpack and babel configs
    because we do not want to hide anything behind CRA or neutrino anymore.
    We need to know everything that is going on with this application.

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,35 @@
-module.exports = {};
+const isDev = process.env.NODE_ENV === "development";
+
+module.exports = {
+  mode: isDev ? "development" : "production",
+  entry: ["./src/index.jsx"],
+  output: {
+    path: __dirname,
+    filename: "./public/bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devtool: "source-map",
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
