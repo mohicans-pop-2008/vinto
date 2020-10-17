ddc0fa478b5c0b2dae442c4a2bb2d8af90f4409f
Kevin Hu <hukxchen+git@gmail.com>
2020-10-17 15:53:45 -0500

    feat: registers event listeners on DOM from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import regeneratorRuntime from 'regenerator-runtime';
+import React, { useEffect, useState } from "react";
+import regeneratorRuntime from "regenerator-runtime";
 
 const createRandomNum = () => Math.floor(Math.random() * 10000);
 
 const loadAndConnect = ({ room }) => {
-  const conference = { room: 'abc' };
-  const localTrack = { type: 'video' };
+  const conference = {
+    room: "abc",
+    on: function (eventType, fn) {
+      console.log("Registering event listener");
+      const elem = document.getElementById("root");
+      elem.addEventListener(eventType, fn);
+    },
+    removeEventListener: function (eventType, fn) {
+      const elem = document.getElementById("root");
+      elem.removeEventListener(eventType, fn);
+    },
+  };
+  const localTrack = { type: "video" };
   return {
     conference,
     localTrack,
@@ -13,34 +24,47 @@ const loadAndConnect = ({ room }) => {
 };
 
 window.setTimeout(mimicAddTrack, 4000);
-function mimicAddTrack () {
-  console.log('MIMIC ADD TRACK FIRED');
-  const trackAdded = new Event('TRACK_ADDED');
-  const root = document.getElementById('root')
-  root.dispatchEvent(trackAdded)
+
+function mimicAddTrack() {
+  console.log("MIMIC ADD TRACK FIRED");
+  const trackAdded = new Event("TRACK_ADDED");
+  const root = document.getElementById("root");
+  root.dispatchEvent(trackAdded);
 }
 
 const App = () => {
-  console.log('RENDERED')
+  console.log("RENDERED");
   const [conference, setConference] = useState(null);
   const [tracks, setTracks] = useState(null);
 
   const connect = (e) => {
     e.preventDefault();
     const { conference: alias, localTrack } = loadAndConnect({
-      room: 'some-default-room',
+      room: "some-default-room",
     });
     setConference(alias);
-    setTracks(localTrack);
+    setTracks([localTrack]);
+  };
+
+  const respondToTrackAdded = (e) => {
+    console.log("target --->", e.target);
+    console.log("React app detects TRACK_ADDED");
+    const trackId = createRandomNum();
+    setTracks([...tracks, { id: trackId }]);
   };
 
   // useEffect(() => {
   //   console.log('This is basically component did mount')
   // }, [])
 
-  // useEffect(() => {
-  //   console.log('Conference changed')
-  // }, [conference])
+  useEffect(() => {
+    console.log("Conference changed");
+    if (!conference) return;
+    conference.on("TRACK_ADDED", respondToTrackAdded);
+    return () => {
+      conference.removeEventListener("TRACK_ADDED", respondToTrackAdded);
+    };
+  }, [conference]);
 
   // useEffect(() => {
   //   mimicAddTrack()
