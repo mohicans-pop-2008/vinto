81dc317953622e859f7ea03622a0199b34a8ed5e
taveershah <tanvirhussein@gmail.com>
2020-10-17 13:58:04 -0400

    feat: state is added to the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import regeneratorRuntime from 'regenerator-runtime';
 
 const createRandomNum = () => Math.floor(Math.random() * 10000);
 
+const loadAndConnect = async ({ room }) => {
+  const conference = { room: 'abc' };
+  const localTrack = { type: 'video' };
+  return {
+    conference,
+    localTrack,
+  };
+};
+
 const App = () => {
-  return <div>{createRandomNum()}</div>;
+  const [conference, setConference] = useState(null);
+  const [tracks, setTracks] = useState(null);
+
+  const connect = async (e) => {
+    e.preventDefault();
+    const { conference, localTrack } = await loadAndConnect({
+      room: 'some-default-room',
+    });
+    setConference(conference);
+    setTracks(localTrack);
+  };
+
+  return (
+    <div>
+      {createRandomNum()}
+      <button type="submit" onClick={connect}>
+        Submit
+      </button>
+      {conference && conference.room}
+      {tracks && tracks.type}
+    </div>
+  );
 };
 
 export default App;
