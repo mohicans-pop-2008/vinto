270e60ba2f74155295bf42c7b19334d0a8d119e1
Chikara <chtakahashi10@gmail.com>
2020-10-17 15:43:51 -0400

    feat: adds a mimicked event firing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import regeneratorRuntime from 'regenerator-runtime';
 
 const createRandomNum = () => Math.floor(Math.random() * 10000);
 
-const loadAndConnect = async ({ room }) => {
+const loadAndConnect = ({ room }) => {
   const conference = { room: 'abc' };
   const localTrack = { type: 'video' };
   return {
@@ -12,18 +12,39 @@ const loadAndConnect = async ({ room }) => {
   };
 };
 
+window.setTimeout(mimicAddTrack, 4000);
+function mimicAddTrack () {
+  console.log('MIMIC ADD TRACK FIRED');
+  const trackAdded = new Event('TRACK_ADDED');
+  const root = document.getElementById('root')
+  root.dispatchEvent(trackAdded)
+}
+
 const App = () => {
+  console.log('RENDERED')
   const [conference, setConference] = useState(null);
   const [tracks, setTracks] = useState(null);
 
-  const connect = async (e) => {
+  const connect = (e) => {
     e.preventDefault();
-    const { conference, localTrack } = await loadAndConnect({
+    const { conference: alias, localTrack } = loadAndConnect({
       room: 'some-default-room',
     });
-    setConference(conference);
+    setConference(alias);
     setTracks(localTrack);
   };
+
+  // useEffect(() => {
+  //   console.log('This is basically component did mount')
+  // }, [])
+
+  // useEffect(() => {
+  //   console.log('Conference changed')
+  // }, [conference])
+
+  // useEffect(() => {
+  //   mimicAddTrack()
+  // }, [tracks])
 
   return (
     <div>
