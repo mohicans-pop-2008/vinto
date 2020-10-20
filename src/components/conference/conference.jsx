ec6e89146890f5f233e832694ec1fd08109019e5
Kevin Hu <hukxchen+git@gmail.com>
2020-10-20 13:08:30 -0500

    style: wrap and calc width for video tiles

--- src/components/conference/conference.jsx
+++ src/components/conference/conference.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import Video from './video';
-import Audio from './audio';
-import styles from './conference.module.css';
+import React from "react";
+import Video from "./video";
+import Audio from "./audio";
+import styles from "./conference.module.css";
 
-const Conference = ({ tracks }) => (
-  <div className={styles.conference} >
-    {Object.keys(tracks)
-      .map((trackKey) => {
-        const element = trackKey.includes("video")
-          ? <Video key={trackKey} track={tracks[trackKey]} />
-          : <Audio key={trackKey} track={tracks[trackKey]} />
+const Conference = ({ tracks }) => {
+
+  const participants = {};
+  Object.keys(tracks).forEach((key) => {
+    let parsedId = key.split('-')
+    let id = parsedId[0]
+    participants[id] = true;
+  });
+  const n = Object.keys(participants).length
+
+  return (
+    <div className={styles.conference}>
+      {Object.keys(tracks).map((trackKey) => {
+        const element = trackKey.includes("video") ? (
+          <Video key={trackKey} track={tracks[trackKey]} n={n} />
+        ) : (
+          <Audio key={trackKey} track={tracks[trackKey]} />
+        );
         return element;
-        }
-      )}
-  </div>
-);
+      })}
+    </div>
+  );
+};
 
 export default Conference;
