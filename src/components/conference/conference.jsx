4e0711ef841f5e1a13778119ee46924058bff616
Kevin Hu <hukxchen+git@gmail.com>
2020-10-21 14:16:51 -0500

    style: introduce calculation of tile size

--- src/components/conference/conference.jsx
+++ src/components/conference/conference.jsx
@@ -1,23 +1,93 @@
-import React from "react";
+import React, { useRef } from "react";
 import Video from "./video";
 import Audio from "./audio";
 import styles from "./conference.module.css";
 
 const Conference = ({ tracks }) => {
-
+  const conferenceDiv = useRef(null); // {current: DOM Element(div)}
   const participants = {};
   Object.keys(tracks).forEach((key) => {
-    let parsedId = key.split('-')
-    let id = parsedId[0]
+    let parsedId = key.split("-");
+    let id = parsedId[0];
     participants[id] = true;
   });
-  const n = Object.keys(participants).length
 
+  /* Layout Math
+   * this is a div container for the video element
+   * it should have a calculated height and width
+   * it should have some fallback content - ID for now, ideally a display name
+   */
+  const participantCount = Object.keys(participants).length;
+  let conferenceDivHeight;
+  let conferenceDivWidth;
+
+  const calculateBestLayout = (conHeight, conWidth) => {
+    console.log(`Vinto: conHeight = ${conHeight} and conWidth = ${conWidth}`);
+
+    if (!conHeight || !conWidth) return;
+    const aspectRatio = 16 / 9; // what is the actual aspectRatio for Jitsi tracks?
+    let bestLayout = {
+      area: 0,
+      cols: 0,
+      rows: 0,
+      width: 0,
+      height: 0,
+    };
+    // brute-force search layout where video occupy the largest area of the container
+    for (let cols = 1; cols <= participantCount; cols++) {
+      console.log("Vinto: is calculating bestLayout");
+      const rows = Math.ceil(participantCount / cols);
+      const hScale = conWidth / (cols * aspectRatio);
+      const vScale = conHeight / rows;
+      let width;
+      let height;
+      if (hScale <= vScale) {
+        width = Math.floor(conWidth / cols);
+        height = Math.floor(width / aspectRatio);
+      } else {
+        height = Math.floor(conHeight / rows);
+        width = Math.floor(height * aspectRatio);
+      }
+      const area = width * height;
+      console.log("Vinto: bestLayout area is --> ", area);
+      if (area > bestLayout.area) {
+        bestLayout = {
+          area,
+          width,
+          height,
+          rows,
+          cols,
+        };
+      }
+    }
+    return bestLayout;
+  };
+
+  let layout;
+  if (conferenceDiv.current !== null) {
+    console.log("Vinto: conferenceDiv", conferenceDiv);
+    conferenceDivHeight = conferenceDiv.current.getBoundingClientRect().height;
+    conferenceDivWidth = conferenceDiv.current.getBoundingClientRect().width;
+    console.log(
+      `Vinto: LAYOUT: computes participantCount: ${participantCount}, conferenceDivHeight: ${conferenceDivHeight}, and conferenceDivWidth: ${conferenceDivWidth}.`
+    );
+    layout = calculateBestLayout(conferenceDivHeight, conferenceDivWidth);
+  }
+
+  // console.log("Vinto: layout on state --> ", layout);
   return (
-    <div className={styles.conference}>
+    <div className={styles.conference} ref={conferenceDiv}>
       {Object.keys(tracks).map((trackKey) => {
         const element = trackKey.includes("video") ? (
-          <Video key={trackKey} track={tracks[trackKey]} n={n} />
+          layout && (
+            <Video
+              key={trackKey}
+              track={tracks[trackKey]}
+              participantCount={participantCount}
+              height={layout.height}
+              width={layout.width}
+            />
+          )
         ) : (
           <Audio key={trackKey} track={tracks[trackKey]} />
         );
--- src/components/conference/video.jsx
+++ src/components/conference/video.jsx
@@ -1,35 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./video.module.css";
 
-const Video = ({ track, n }) => {
+const Video = ({ track, participantCount, height, width }) => {
   console.log(
     `=====> Video track ${track.getId()} is Muted: ${track.isMuted()} <=====`
   );
 
-  /**
-   * Layout
-   *
-   * this is a div container for the video element
-   * it should have a calculated height and width
-   * it should have some fallback content - ID for now, ideally a display name
-   */
-  const aspectRatio = 16 / 9; // what is the actual aspectRatio for Jitsi tracks?
-
   const fallback = track.getParticipantId();
   return (
-    <>
-      <div style={{ width: `calc(100% / ${n})` }} className={styles.videoTile}>
-        {track.isMuted() ? (
-          <h3 color="white">{fallback}</h3>
-        ) : (
-          <video
-            className={styles.videoTrack}
-            autoPlay="1"
-            ref={(ref) => ref && track.attach(ref)}
-          />
-        )}
-      </div>
-    </>
+    <div style={{ height: height, width: width }} className={styles.videoTile}>
+      {track.isMuted() ? (
+        <h3 color="white">{fallback}</h3>
+      ) : (
+        <video
+          className={styles.videoTrack}
+          autoPlay="1"
+          ref={(ref) => ref && track.attach(ref)}
+        />
+      )}
+    </div>
   );
 };
 
