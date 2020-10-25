08a9da199790c5bf96fd0b68591cc9763a96d4af
Chikara <chtakahashi10@gmail.com>
2020-10-25 18:26:04 -0400

    feat: adds a modal for hands raised, and disables the hands button if there are no raised hands.

--- src/components/sidebar/hands.jsx
+++ src/components/sidebar/hands.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import HandsModal from './handsModal';
 import { Hand } from "@styled-icons/entypo/Hand";
 import styles from "./hand.module.css";
 
 const Hands = () => {
+  const [modalShowing, setModalShowing] = useState(false)
   const onClick = () => {
-    console.log(
-      "==================THIS WILL EVENTUALLY BE REPLACED WITH expanding/collapsing an element to show whos hands are raised!=================="
-    );
+    setModalShowing(!modalShowing)
   };
   const raisedHands = useSelector((state) => state.raisedHands);
   return (
-    <button
-      type="button"
-      className={raisedHands.length ? styles.handsRaised : styles.none}
-      onClick={onClick}
-    >
-      <Hand size="24" title="Hands Raised" />
-      {`${raisedHands.length}`}
-    </button>
+    <>
+      <button
+        type="button"
+        className={raisedHands.length ? styles.handsRaised : styles.none}
+        disabled={!raisedHands.length}
+        onClick={onClick}
+      >
+        <Hand size="24" title="Hands Raised" />
+        {`${raisedHands.length}`}
+      </button>
+      {modalShowing ? <HandsModal /> : null}
+    </>
   );
 };
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,19 +2,17 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import engagementScore from './engagementScore';
-import name from './name';
 import participants from './participants';
 import raisedHands from './raisedHands';
 
 const reducer = combineReducers({
-  engagementScore, name, participants, raisedHands,
+  engagementScore, participants, raisedHands,
 });
 const middleware = applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }));
 const store = createStore(reducer, middleware);
 
 export default store;
 export * from './engagementScore';
-export * from './name';
 export * from './participants';
 export * from './raisedHands';
 
