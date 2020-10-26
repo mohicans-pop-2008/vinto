5d980226240c2052e6b5668abbdd78fd84a93332
Kevin Hu <hukxchen+git@gmail.com>
2020-10-26 07:21:26 -0500

    sty: use X for leave

--- src/components/meetingControls/leave.jsx
+++ src/components/meetingControls/leave.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import { X } from "@styled-icons/feather";
+import styles from "./leave.module.css";
 
 const Leave = ({ localTracks }) => {
   const leave = async () => {
-    localTracks
-      .forEach(async (track) => await track.dispose());
+    localTracks.forEach(async (track) => await track.dispose());
     await conference.leave();
     setConference(null);
     setTracks({});
     setName("");
     setId("");
   };
-  return <button onClick={leave}>Leave Conference</button>;
+  return (
+    <Button
+      onClick={leave}
+      className={styles.leave}
+      variant="outlined"
+      startIcon={<X size="24" />}
+    >
+      Leave
+    </Button>
+  );
 };
 
 export default Leave;
