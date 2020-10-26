680531fb5995650f8ed68e4a347738146e5acffe
Kevin Hu <hukxchen+git@gmail.com>
2020-10-26 07:22:05 -0500

    sty: use Cast for screencast

--- src/components/meetingControls/screenShare.jsx
+++ src/components/meetingControls/screenShare.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import buttonStyle from '../style';
+import Button from '@material-ui/core/Button'
+import { Cast } from '@styled-icons/feather'
 
 const ScreenShare = () => (
-  <button type="button" style={buttonStyle}>
-    ScreenShare Button
-  </button>
+  <Button type="button" startIcon={<Cast size="24" />}>Share Screen</Button>
 );
 
 export default ScreenShare;
