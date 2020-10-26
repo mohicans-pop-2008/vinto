a35bd5116ba05f91679a92a2da7850b3c30701fa
Kevin Hu <hukxchen+git@gmail.com>
2020-10-26 07:21:48 -0500

    sty: use Hand and HandLeft for raise hand

--- src/components/meetingControls/raiseHand.jsx
+++ src/components/meetingControls/raiseHand.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { raiseHand, lowerHand } from '../../store';
-import buttonStyle from '../style';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { raiseHand, lowerHand } from "../../store";
+import Button from "@material-ui/core/Button";
+import { Hand } from "@styled-icons/entypo";
+import { HandLeft } from "@styled-icons/ionicons-outline";
+import buttonStyle from "../style";
 
 const RaiseHand = ({ name, id }) => {
   const [handRaised, setHandRaised] = useState(false);
@@ -16,9 +19,21 @@ const RaiseHand = ({ name, id }) => {
     setHandRaised(!handRaised);
   };
   return (
-    <button type="button" style={buttonStyle} onClick={onClick}>
-      {handRaised ? 'Lower my hand' : 'Raise my hand'}
-    </button>
+    <>
+      {handRaised ? (
+        <Button
+          type="button"
+          onClick={onClick}
+          startIcon={<HandLeft size="24" />}
+        >
+          Lower Hand
+        </Button>
+      ) : (
+        <Button type="button" onClick={onClick} endIcon={<Hand size="24" />}>
+          Raise Hand
+        </Button>
+      )}
+    </>
   );
 };
 
